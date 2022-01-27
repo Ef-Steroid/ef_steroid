11e2dcb68d8f1657c29d566189ab8254aa0550fa
Kah Wai <taboosun1996@gmail.com>
2022-01-27 18:39:08 +0800

    Force moving artifacts even the artifacts directory is not empty

--- tools/CsprojTool/publish.ps1
+++ tools/CsprojTool/publish.ps1
@@ -6,7 +6,7 @@ $ArtifactDirectory = "..\..\windows\artifacts\CsprojTool\win-x64\"
 
 # Create target directory
 New-Item $ArtifactDirectory -ItemType Directory -Force
-mv bin\Release\net6.0\win-x64\publish\ $ArtifactDirectory
+mv bin\Release\net6.0\win-x64\publish\ $ArtifactDirectory -Force
 echo "Done moving artifact to Flutter Windows"
 
 echo "Done..."
